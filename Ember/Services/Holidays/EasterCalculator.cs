b71f40f7b4f4cdf8b5308c05f4ec6e29f97698ee
AnotherEpigone <mikestrauss612@gmail.com>
2021-07-05 20:54:20 -0400

    added more information to the comments and added test cases for Easter calculation

--- Ember/Services/Holidays/EasterCalculator.cs
+++ Ember/Services/Holidays/EasterCalculator.cs
@@ -7,8 +7,9 @@ namespace Ember.Services.Holidays
         public DateTime Get(int year)
         {
             // Based on the "New Scientist" algorithm published 1961.
-            // Golden number = lunar cycle year.
-            var goldenNumber = year % 19;
+            // note that the single-letter variable names are as-written in
+            // the published algorithm.
+            var goldenNumber = year % 19; // position in 19 year lunar cycle.
             var century = year / 100;
             var shortYear = year % 100;
 
@@ -18,15 +19,19 @@ namespace Ember.Services.Holidays
             // leftover centuries within the 4 century period.
             var e = century % 4;
 
-            // ?????
+            // not sure
             var g = ((8 * century) + 13) / 25;
+
+            // h = days between the spring equinox and the following full moon.
             var h = ((19 * goldenNumber) + century - d - g + 15) % 30;
+
+            // not sure
             var i = shortYear / 4;
             var k = shortYear % 4;
             var l = (32 + (2 * e) + (2 * i) - h - k) % 7;
             var m = (goldenNumber + (11 * h) + (19 * l)) / 433;
 
-            // now we get the month and day based on the insane magic numbers above!
+            // now we get the month and day based on the magic numbers above
             var month = (h + l - (7 * m) + 90) / 25;
             var day = (h + l - (7 * m) + (33 * month) + 19) % 32;
 
--- EmberTests/EasterCalculatorTests.cs
+++ EmberTests/EasterCalculatorTests.cs
@@ -7,8 +7,11 @@ namespace EmberTests
     public class EasterCalculatorTests
     {
         [Theory]
+        [InlineData(1900, 4, 15)]
         [InlineData(1961, 4, 2)]
+        [InlineData(2020, 4, 12)]
         [InlineData(2021, 4, 4)]
+        [InlineData(2022, 4, 17)]
         public void Get(int year, int month, int day)
         {
             var expected = new DateTime(year, month, day);
